d52a78fc030b0ed4f0b41db3d7fa57f39ef30612
Lincoln Rodrigues <lincolnrodrs@gmail.com>
2020-08-20 10:28:31 -0300

    add uri to error message (#28)

--- spec/eezee/errors/request_error_spec.rb
+++ spec/eezee/errors/request_error_spec.rb
@@ -4,8 +4,9 @@ require 'spec_helper'
 
 RSpec.describe Eezee::RequestError, type: :model do
   describe '#initialize' do
-    subject { described_class.new(response) }
+    subject { described_class.new(request, response) }
 
+    let(:request) { build :request }
     let(:response) { Eezee::Response.new(nil) }
 
     before do
@@ -14,14 +15,15 @@ RSpec.describe Eezee::RequestError, type: :model do
     end
 
     it { is_expected.to be_a(described_class) }
-    it { is_expected.to have_attributes(message: 'CODE: 400 - BODY: {"error":"some error"}') }
+    it { is_expected.to have_attributes(message: 'CODE: 400 - URI: https://www.linqueta.com/users/10/addresses/15?state=Sao Paulo&country=Brazil - BODY: {"error":"some error"}') } # rubocop:disable Metrics/LineLength
   end
 
   describe '#log' do
     subject { error.log }
 
-    let(:error) { described_class.new(response) }
+    let(:error) { described_class.new(request, response) }
     let(:response) { Eezee::Response.new(nil) }
+    let(:request) { build :request }
 
     before do
       allow(response).to receive(:body).and_return(error: 'some error')
