528562778d9766d01a9eaabe46145bdbf9dcc6e7
Alipio de Paula <alipiodepaula@gmail.com>
2023-08-23 14:55:54 -0300

    Add retry middleware to requester

--- lib/eezee/client/requester.rb
+++ lib/eezee/client/requester.rb
@@ -8,6 +8,16 @@ module Eezee
     module Requester
       METHODS = %i[get post patch put delete].freeze
 
+      CONNECTION_ERRORS = [
+        EOFError,
+        Errno::ECONNRESET,
+        Errno::ETIMEDOUT,
+        SocketError,
+        Errno::ECONNREFUSED,
+        'Timeout::Error',
+        Faraday::TimeoutError
+      ].freeze
+
       def self.extended(base)
         METHODS.each do |method|
           base.send(
@@ -76,14 +86,24 @@ module Eezee
         end
       end
 
-      def faraday_client_options!(config, request)
+      def faraday_client_options!(config, request) # rubocop:disable Metrics
         config.request :url_encoded if request.url_encoded
+        config.use Faraday::Request::Retry, exceptions: CONNECTION_ERRORS, **retry_options
         config.use(Faraday::Response::RaiseError) if request.raise_error
         config.headers = request.headers if request.headers
         config.options[:open_timeout] = request.open_timeout if request.open_timeout
         config.options[:timeout] = request.timeout if request.timeout
         config.adapter(Faraday.default_adapter)
         config.use(:ddtrace, request.ddtrace) if request.ddtrace.any?
+      end
+
+      def retry_options
+        {
+          interval: 0.5,
+          interval_randomness: 0.5,
+          max_interval: 60,
+          backoff_factor: 2
+        }
       end
     end
   end
